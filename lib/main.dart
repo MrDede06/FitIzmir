6d8d44ca0dbf51de90d7a05392d729844c2edb85
Ozgur Dede <Ozgur.Dede@gds.ey.com>
2020-12-20 16:19:34 +0100

    Adaptive size added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:fitizmir/screens/auth_screen.dart';
 import 'package:fitizmir/screens/home_screen.dart';
@@ -9,6 +9,10 @@ import 'package:fitizmir/screens/hazir_diyetler_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
   runApp(MyApp());
 }
 
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fitizmir/screens/hazir_diyetler_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
+    final mediaQuery = MediaQuery.of(context);
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
@@ -14,103 +13,119 @@ class HomeScreen extends StatelessWidget {
         bottom: false,
         left: true,
         right: true,
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Container(
+        child: Container(
+          height: mediaQuery.size.height,
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                height: (mediaQuery.size.height -
+                    mediaQuery.padding.top) *
+                    0.25
+                ,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
 //                  color: Theme.of(context).accentColor,
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(vertical: 12, horizontal: 4),
-                  child: Image(
-                    image: AssetImage(
-                        'assets/images/logo1.png'),
-                    width: 160,
-                    height: 160,
-                  ),
-                ),
-
-                Container(
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-                  child: ClipOval(
-
-                    child: Image(
-                      image: AssetImage(
-                        'assets/images/fitIzmirresim.png'),
-                      fit: BoxFit.cover,
-                    width: 160,
-                    height: 160,
-                  ),
-                  ),
-                ),
-              ],
-            ),
-
-
-            GridView.count(
-              crossAxisCount: 2,
-              crossAxisSpacing: 1,
-              mainAxisSpacing: 1,
-              shrinkWrap: true,
-              padding: const EdgeInsets.all(1),
-              children: <Widget>[
-                Container(
-                  child: InkWell(
-                    onTap: (){print("profil");},
-                    child: Image(
-                      image: AssetImage("assets/images/profil.png"),
+                      color: Colors.white,
+                      margin: EdgeInsets.symmetric(vertical: 12, horizontal: 4),
+                      child: Image(
+                        image: AssetImage('assets/images/logo1.png'),
+                        width: 160,
+                        height: 160,
+                      ),
                     ),
-                  ),
-                ),
-                Container(
-                  child: InkWell(
-                    onTap: (){print("profil");},
-                    child: Image(
-                      image: AssetImage("assets/images/ekibimiz.jpg"),
+                    Container(
+                      color: Colors.white,
+                      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                      child: ClipOval(
+                        child: Image(
+                          image: AssetImage('assets/images/fitIzmirresim.png'),
+                          fit: BoxFit.cover,
+                          width: 160,
+                          height: 160,
+                        ),
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-                Container(
-                  child: InkWell(
-                    onTap: (){print("profil");},
-                    child: Image(
-                      image: AssetImage("assets/images/blog.jpg"),
+              ),
+              Container(
+                height: (mediaQuery.size.height -
+              mediaQuery.padding.top) *
+                  0.75,
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 1,
+                  mainAxisSpacing: 1,
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.all(1),
+                  children: <Widget>[
+                    Container(
+                      child: InkWell(
+                        onTap: () {
+                          print("profil");
+                        },
+                        child: Image(
+                          image: AssetImage("assets/images/profil.png"),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                Container(
-                  child: InkWell(
-                    onTap: (){
-
-                      },
-                    child: Image(
-                      image: AssetImage("assets/images/onlinediyet.jpg"),
+                    Container(
+                      child: InkWell(
+                        onTap: () {
+                          print("profil");
+                        },
+                        child: Image(
+                          image: AssetImage("assets/images/ekibimiz.jpg"),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                Container(
-                  child: InkWell(
-                    onTap: (){Navigator.of(context).pushNamed(HazirDiyetlerScreen.routeName);},
-                    child: Image(
-                      image: AssetImage("assets/images/ozeldiyetler.jpg"),
+                    Container(
+                      child: InkWell(
+                        onTap: () {
+                          print("profil");
+                        },
+                        child: Image(
+                          image: AssetImage("assets/images/blog.jpg"),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                Container(
-                  child: InkWell(
-                    onTap: (){print("profil");},
-                    child: Image(
-                      image: AssetImage("assets/images/kilokoruma.jpg"),
+                    Container(
+                      child: InkWell(
+                        onTap: () {},
+                        child: Image(
+                          image: AssetImage("assets/images/onlinediyet.jpg"),
+                        ),
+                      ),
                     ),
-                  ),
+                    Container(
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.of(context)
+                              .pushNamed(HazirDiyetlerScreen.routeName);
+                        },
+                        child: Image(
+                          image: AssetImage("assets/images/ozeldiyetler.jpg"),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: InkWell(
+                        onTap: () {
+                          print("profil");
+                        },
+                        child: Image(
+                          image: AssetImage("assets/images/kilokoruma.jpg"),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
