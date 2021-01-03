c5f87f310a6be1626a7958ec345f69c5441df828
Ozgur Dede <Ozgur.Dede@gds.ey.com>
2021-01-03 22:46:30 +0100

    trying animation

--- lib/screens/hazir_diyet_detay_screen.dart
+++ lib/screens/hazir_diyet_detay_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_bounce/flutter_bounce.dart';
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -6,8 +7,16 @@ import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-class HazirDiyetlerDetayScreen extends StatelessWidget {
+class HazirDiyetlerDetayScreen extends StatefulWidget {
   static const routeName = '/hazirdiyetlerdetay';
+
+  @override
+  _HazirDiyetlerDetayScreenState createState() =>
+      _HazirDiyetlerDetayScreenState();
+}
+
+class _HazirDiyetlerDetayScreenState extends State<HazirDiyetlerDetayScreen> {
+  bool selected = false;
 
   Widget build(BuildContext context) {
     final HazirDiyetScreenArgs args = ModalRoute.of(context).settings.arguments;
@@ -45,10 +54,14 @@ class HazirDiyetlerDetayScreen extends StatelessWidget {
               height: (mediaQuery.size.height - mediaQuery.padding.top) * 0.06,
               child: (args.fiyat == 0)
                   ? InkWell(
-                      onTap: ()async{
+                      onTap: () async {
+                        setState(() {
+                          selected = !selected;
+                        });
                         final status = await Permission.storage.request();
-                        if(status.isGranted){
-                          final externalDir = await getExternalStorageDirectory();
+                        if (status.isGranted) {
+                          final externalDir =
+                              await getExternalStorageDirectory();
                           final id = await FlutterDownloader.enqueue(
                             url: args.pdf,
                             savedDir: externalDir.path,
@@ -56,47 +69,58 @@ class HazirDiyetlerDetayScreen extends StatelessWidget {
                             showNotification: true,
                             openFileFromNotification: true,
                           );
-                        }else{
+                        } else {
                           print("Permission deined");
                         }
+                        setState(() {
+                          selected = !selected;
+                        });
                       },
-                      child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.blueGrey[50],
-                            border: Border.all(color: Colors.grey),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          margin: EdgeInsets.all(2),
-                          padding: EdgeInsets.all(10),
-                          width: 80,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                          Text(
-                            "İndir",
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.black,
-                            ),
-                          ),
-                              SizedBox(
-                                width: 4,
-                              ),
-                              Icon(
-                                Icons.file_download,
+                      child: AnimatedContainer(
+                        duration: Duration(milliseconds: 100),
+                        curve: Curves.fastOutSlowIn,
+                        decoration: BoxDecoration(
+                          color: selected ?  Colors.blueGrey : Colors.blueGrey[50],
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        margin: EdgeInsets.all(2),
+                        padding: EdgeInsets.all(10),
+                        width: 80,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              "İndir",
+                              style: TextStyle(
+                                fontSize: 16,
                                 color: Colors.black,
-                                size: 16,
                               ),
-    ],
-    ),
+                            ),
+                            SizedBox(
+                              width: 4,
+                            ),
+                            Icon(
+                              Icons.file_download,
+                              color: Colors.black,
+                              size: 16,
+                            ),
+                          ],
+                        ),
                       ),
                     )
                   : InkWell(
-                      onTap: () => print("Satin al"),
-                      splashColor: Colors.blueGrey,
-                      child: Container(
+                      onTap: () => {setState(() {
+                        selected = !selected;
+                      }),
+                      },
+                      child: AnimatedContainer(
+                       // width: selected ? 200.0 : 100.0,
+                       // height: selected ? 100.0 : 200.0,
+                        duration: Duration(milliseconds: 100),
+                        curve: Curves.fastOutSlowIn,
                         decoration: BoxDecoration(
-                          color: Colors.blueGrey[50],
+                          color: selected ?  Colors.blueGrey : Colors.blueGrey[50],
                           border: Border.all(color: Colors.grey),
                           borderRadius: BorderRadius.circular(10),
                         ),
--- lib/screens/hazir_diyetler_screen.dart
+++ lib/screens/hazir_diyetler_screen.dart
@@ -1,23 +1,29 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fitizmir/screens/hazir_diyet_detay_screen.dart';
 
-class HazirDiyetScreenArgs{
+class HazirDiyetScreenArgs {
   final String isim;
   final String aciklama;
   final bool bedava;
   final double fiyat;
   final String pdf;
   final String resim;
-  HazirDiyetScreenArgs(this.isim, this.fiyat, this.aciklama, this.bedava, this.pdf, this.resim);
+  HazirDiyetScreenArgs(
+      this.isim, this.fiyat, this.aciklama, this.bedava, this.pdf, this.resim);
 }
 
-class HazirDiyetlerScreen extends StatelessWidget {
+class HazirDiyetlerScreen extends StatefulWidget {
   static const routeName = '/hazirdiyetler';
 
+  @override
+  _HazirDiyetlerScreenState createState() => _HazirDiyetlerScreenState();
+}
+
+class _HazirDiyetlerScreenState extends State<HazirDiyetlerScreen> {
+  bool selected = false;
   Widget build(BuildContext context) {
     final screen_width = MediaQuery.of(context).size.width;
     return Scaffold(
@@ -28,100 +34,101 @@ class HazirDiyetlerScreen extends StatelessWidget {
             style: GoogleFonts.pacifico(),
           )),
       body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection("HazirPaketler").snapshots(),
-        builder: (ctx, categorySnapshot){
-          if(categorySnapshot.connectionState == ConnectionState.waiting) return CircularProgressIndicator();
+        stream:
+            FirebaseFirestore.instance.collection("HazirPaketler").snapshots(),
+        builder: (ctx, categorySnapshot) {
+          if (categorySnapshot.connectionState == ConnectionState.waiting)
+            return CircularProgressIndicator();
           final paketler = categorySnapshot.data.documents;
-              return GridView.builder(
-                primary: false,
-                padding: const EdgeInsets.all(2),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisSpacing: 2, mainAxisSpacing: 2, crossAxisCount: 2),
-                itemCount: paketler.length,
-                itemBuilder: (ctx, index) =>
-                    InkWell(
-                      onTap: () => {
-                      Navigator.pushNamed(
-                      ctx,
-                      HazirDiyetlerDetayScreen.routeName,
-                      arguments: HazirDiyetScreenArgs(
-                          paketler[index]['isim'],
-                          paketler[index]['fiyat'].toDouble(),
-                          paketler[index]['aciklama'],
-                          paketler[index]['bedava'],
-                          paketler[index]['pdf'],
+          return GridView.builder(
+            primary: false,
+            padding: const EdgeInsets.all(2),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisSpacing: 2, mainAxisSpacing: 2, crossAxisCount: 2),
+            itemCount: paketler.length,
+            itemBuilder: (ctx, index) => InkWell(
+              onTap: () => {
+                Navigator.pushNamed(
+                  ctx,
+                  HazirDiyetlerDetayScreen.routeName,
+                  arguments: HazirDiyetScreenArgs(
+                    paketler[index]['isim'],
+                    paketler[index]['fiyat'].toDouble(),
+                    paketler[index]['aciklama'],
+                    paketler[index]['bedava'],
+                    paketler[index]['pdf'],
+                    paketler[index]['resim'],
+                  ),
+                )
+              },
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                elevation: 4,
+                margin: EdgeInsets.all(5),
+                child: Stack(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(15),
+                          topRight: Radius.circular(15),
+                          bottomLeft: Radius.circular(15),
+                          bottomRight: Radius.circular(15),
+                        ),
+                        child: Image.network(
                           paketler[index]['resim'],
-                      ),
-                      )
-                        },
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15),
+                          height: 250,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
                         ),
-                        elevation: 4,
-                        margin: EdgeInsets.all(5),
-                        child: Stack(
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(15),
-                                topRight: Radius.circular(15),
-                                bottomLeft: Radius.circular(15),
-                                bottomRight: Radius.circular(15),
-                              ),
-                              child: Image.network(
-                                paketler[index]['resim'],
-                                height: 250,
-                                width: double.infinity,
-                                fit: BoxFit.cover,
-                              ),
+                      ),
+                      Positioned(
+                        bottom: 20,
+                        child: SizedBox(
+                          width: (screen_width - 10) / 2,
+                          child: Container(
+                            color: Colors.black54,
+                            padding: EdgeInsets.symmetric(
+                              vertical: 5,
+                              horizontal: 5,
                             ),
-                            Positioned(
-                              bottom: 20,
-                              child: SizedBox(
-                                width: (screen_width - 10) / 2,
-                                child: Container(
-                                  color: Colors.black54,
-                                  padding: EdgeInsets.symmetric(
-                                    vertical: 5,
-                                    horizontal: 5,
+                            child: Column(
+                              children: <Widget>[
+                                Text(
+                                  paketler[index]['isim'],
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.white,
+                                    fontFamily: 'RobotoCondensed',
+                                    fontWeight: FontWeight.bold,
                                   ),
-                                  child: Column(
-                                    children: <Widget>[
-                                      Text(
-                                        paketler[index]['isim'],
-                                        style: TextStyle(
-                                          fontSize: 12,
-                                          color: Colors.white,
-                                          fontFamily: 'RobotoCondensed',
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                        softWrap: true,
-                                        overflow: TextOverflow.fade,
-                                      ),
-                                      Text(
-                                        (paketler[index]['fiyat'] == 0) ?
-                                        "BEDAVA!" : paketler[index]['fiyat']
-                                            .toString() + '₺',
-                                        style: TextStyle(
-                                          fontSize: 16,
-                                          color: Colors.green,
-                                        ),
-                                        softWrap: true,
-                                        overflow: TextOverflow.fade,
-                                      ),
-                                    ],
-                                  ),
-
+                                  softWrap: true,
+                                  overflow: TextOverflow.fade,
                                 ),
-                              ),
+                                Text(
+                                  (paketler[index]['fiyat'] == 0)
+                                      ? "BEDAVA!"
+                                      : paketler[index]['fiyat'].toString() +
+                                          '₺',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.green,
+                                  ),
+                                  softWrap: true,
+                                  overflow: TextOverflow.fade,
+                                ),
+                              ],
                             ),
-                          ],
+                          ),
                         ),
                       ),
-                    ),
-              );
+                    ],
+                  ),
 
+              ),
+            ),
+          );
         },
       ),
     );
